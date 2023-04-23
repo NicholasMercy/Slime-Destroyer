db344244f52f607de0f00dbc8e5f92587422a7d4
Nicholas <NicholasNaicker@hotmail.com>
2023-04-23 14:03:23 +0200

    Done With Movement Stuff for Unit 3

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float currentSpeed;
+    private float zBound = 24f;
+    private float xBound = 24f;
+
+    private Rigidbody playerRb;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        playerRb = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        ConstraintPlayerMove();
+        MovePlayer();
+
+    }
+
+    void MovePlayer()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        Vector3 moveDir = new Vector3(horizontalInput, 0, verticalInput).normalized;
+        transform.position += moveDir * currentSpeed * Time.deltaTime;
+    }
+    void ConstraintPlayerMove()
+    {
+        //restrictions
+        if (transform.position.z < -zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zBound);
+        }
+        if (transform.position.z > zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zBound);
+        }
+        if (transform.position.x < -xBound)
+        {
+            transform.position = new Vector3(-xBound, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xBound)
+        {
+            transform.position = new Vector3(xBound, transform.position.y, transform.position.z);
+        }
+    }
+
+}
